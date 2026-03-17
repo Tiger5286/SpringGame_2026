d2d7d46dcd608a222bd40a47239db71c2fa30699
Tiger5286 <210645128+Tiger5286@users.noreply.github.com>
2026-03-17 22:22:41 +0900

    プレイヤーが移動できるように

--- Project/source/GameObjects/Player.cpp
+++ Project/source/GameObjects/Player.cpp
@@ -1,4 +1,10 @@
 ﻿#include "Player.h"
+#include "../Geometry.h"
+
+namespace
+{
+	constexpr float kMoveSpeed = 5.0f;
+}
 
 Player::Player(Input& input) :
 	m_input(input)
@@ -19,7 +25,17 @@ void Player::End()
 
 void Player::Update()
 {
-	
+	// 左スティックの入力を取得
+	auto leftStick = m_input.GetStickInput(LR::Left);
+
+	// 入力に応じてプレイヤーの位置を更新
+	m_pos += Vector3(leftStick.x, 0.0f, leftStick.y) * kMoveSpeed;
+
+	// プレイヤーの位置に応じた行列を生成
+	Matrix4x4 translateMtx = Matrix4x4::GetTranslateMatrix(m_pos);
+
+	// プレイヤーのモデルに行列を適用
+	MV1SetMatrix(m_modelHandle, translateMtx.ToDxLib());
 }
 
 void Player::Draw()
