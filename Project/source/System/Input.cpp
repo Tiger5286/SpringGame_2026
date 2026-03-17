b2dce783c80910a30e415ef971adeb306d4237d0
Tiger5286 <toratiger5286@gmail.com>
2026-03-17 16:02:52 +0900

    Inputクラス仮完成

--- Project/source/System/Input.cpp
+++ Project/source/System/Input.cpp
@@ -13,26 +13,44 @@ void Input::Update()
 {
 	m_prevPadInput = m_nowPadInput;
 
-	auto check = GetJoypadXInputState(DX_INPUT_PAD1, &m_nowPadInput);
-	assert(check != -1 && "ì¸óÕÇ™ê≥ÇµÇ≠éÊìæÇ≈Ç´Ç‹ÇπÇÒÇ≈ÇµÇΩ");
+	GetJoypadXInputState(DX_INPUT_PAD1, &m_nowPadInput);
 }
 
 bool Input::IsPressed(int button)
 {
-	return false;
+	return m_nowPadInput.Buttons[button];
 }
 
 bool Input::IsTriggerd(int button)
 {
-	return false;
+	return m_nowPadInput.Buttons[button] && !m_prevPadInput.Buttons[button];
 }
 
 StickInput Input::GetStickInput(LR lr)
 {
-	return StickInput();
+	StickInput stick;
+	if (lr == LR::Left)
+	{
+		stick.horizontal = m_nowPadInput.ThumbLX;
+		stick.vertical = m_nowPadInput.ThumbLY;
+	}
+	else if (lr == LR::Right)
+	{
+		stick.horizontal = m_nowPadInput.ThumbRX;
+		stick.vertical = m_nowPadInput.ThumbRY;
+	}
+	return stick;
 }
 
 unsigned char Input::GetTriggerInput(LR lr)
 {
+	if (lr == LR::Left)
+	{
+		return m_nowPadInput.LeftTrigger;
+	}
+	else if (lr == LR::Right)
+	{
+		return m_nowPadInput.RightTrigger;
+	}
 	return 0;
 }
