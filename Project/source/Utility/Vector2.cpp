5c3641ceb1ad6c5fc1a92d62be884a3da47bfbe1
Tiger5286 <210645128+Tiger5286@users.noreply.github.com>
2026-03-17 21:45:11 +0900

    Vector2クラス実装

---
+++ Project/source/Utility/Vector2.cpp
@@ -0,0 +1,105 @@
+﻿#include "Vector2.h"
+#include <cmath>
+
+Vector2::Vector2() :
+	x(0.0f),
+	y(0.0f)
+{
+}
+
+Vector2::Vector2(float x, float y) :
+	x(x),
+	y(y)
+{
+}
+
+Vector2::~Vector2()
+{
+}
+
+float Vector2::SquareLength() const
+{
+	return x * x * y * y;
+}
+
+float Vector2::Length() const
+{
+	return sqrtf(SquareLength());
+}
+
+void Vector2::Normalize()
+{
+	float len = Length();
+	if (len == 0.0f)
+	{
+		x = 0.0f;
+		y = 0.0f;
+	}
+	else
+	{
+		x /= len;
+		y /= len;
+	}
+}
+
+Vector2 Vector2::Normalized() const
+{
+	float len = Length();
+	if (len == 0.0f)
+	{
+		return { 0.0f,0.0f };
+	}
+	else
+	{
+		return { x / len,y / len };
+	}
+}
+
+Vector2 Vector2::operator+(const Vector2 vec) const
+{
+	return { x + vec.x,y + vec.y };
+}
+
+void Vector2::operator+=(const Vector2 vec)
+{
+	x += vec.x;
+	y += vec.y;
+}
+
+Vector2 Vector2::operator-(const Vector2 vec) const
+{
+	return { x - vec.x,y - vec.y };
+}
+
+void Vector2::operator-=(const Vector2 vec)
+{
+	x -= vec.x;
+	y -= vec.y;
+}
+
+Vector2 Vector2::operator*(const float scale) const
+{
+	return { x * scale,y * scale };
+}
+
+void Vector2::operator*=(const float scale)
+{
+	x *= scale;
+	y *= scale;
+}
+
+Vector2 Vector2::operator/(const float scale) const
+{
+	return { x / scale,y / scale };
+}
+
+void Vector2::operator/=(const float scale)
+{
+	x /= scale;
+	y /= scale;
+}
+
+Vector2 Vector2::operator-() const
+{
+	return { -x,-y };
+}
