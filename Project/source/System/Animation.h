98fe165af8fc58734f0de2753def69cd592b69e6
Tiger5286 <toratiger5286@gmail.com>
2026-03-18 16:49:58 +0900

    Animationクラス実装中

--- Project/source/System/Animation.h
+++ Project/source/System/Animation.h
@@ -9,6 +9,8 @@ public:
 	void End();
 	void Update();
 
+	void ChangeAnim(int animHandle);
+
 private:
 	// アニメーションを行うモデルのハンドル
 	int m_modelHandle = -1;
@@ -16,4 +18,11 @@ private:
 	// アニメーションのハンドル
 	int m_currentAnimHandle = -1;
 	int m_lastAnimHandle = -1;
+	
+	// アニメーション管理
+	float m_currentAnimTime = 0.0f;
+	float m_lastAnimTime = 0.0f;
+
+	// アニメーションの切り替えフレーム数
+	int m_animChangeFrame = 0;
 };
