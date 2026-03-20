d2d081eabb9b5792115b91e06c74c65287bec2f9
Tiger5286 <210645128+Tiger5286@users.noreply.github.com>
2026-03-20 23:09:24 +0900

    コリジョンマネージャー実装用意

---
+++ Project/CollisionManager.cpp
@@ -0,0 +1,26 @@
+﻿#include "CollisionManager.h"
+
+CollisionManager::CollisionManager()
+{
+}
+
+CollisionManager::~CollisionManager()
+{
+}
+
+void CollisionManager::Init()
+{
+}
+
+void CollisionManager::End()
+{
+}
+
+void CollisionManager::Update()
+{
+}
+
+void CollisionManager::RegisterSphere(Sphere& sphere)
+{
+	m_spheres.push_back(sphere);
+}
