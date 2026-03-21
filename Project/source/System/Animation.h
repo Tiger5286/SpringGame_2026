b10bf2895ede6ed581d04915d227374e01a87711
Tiger5286 <210645128+Tiger5286@users.noreply.github.com>
2026-03-21 10:20:58 +0900

    Animationクラスにアニメーションの再生速度を変更できる機能を追加

--- Project/source/System/Animation.h
+++ Project/source/System/Animation.h
@@ -10,9 +10,11 @@ public:
 	void Init(int modelHandle,std::wstring animName);
 	void Update();
 
-	void ChangeAnim(std::wstring animName);
-
+	void ChangeAnim(std::wstring animName, float animSpeed = kDefaultAnimSpeed);
 private:
+	// デフォルトのアニメーションの進行速度
+	static constexpr float kDefaultAnimSpeed = 0.5f;
+
 	// アニメーションを行うモデルのハンドル
 	int m_modelHandle = -1;
 
@@ -21,6 +23,7 @@ private:
 	int m_lastAnimHandle = -1;
 	
 	// アニメーション管理
+	float m_animSpeed = kDefaultAnimSpeed;
 	float m_currentAnimTime = 0.0f;
 	float m_lastAnimTime = 0.0f;
 
