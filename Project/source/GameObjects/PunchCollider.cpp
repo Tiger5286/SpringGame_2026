e763076fe6ea546d2ccb74aeb558f94e9a4b25c1
Tiger5286 <210645128+Tiger5286@users.noreply.github.com>
2026-03-21 10:37:34 +0900

    PunchCollider実装準備

---
+++ Project/source/GameObjects/PunchCollider.cpp
@@ -0,0 +1,38 @@
+﻿#include "PunchCollider.h"
+
+namespace
+{
+	constexpr float kSphereRadius = 100.0f;
+}
+
+PunchCollider::PunchCollider():
+	GameObject(kSphereRadius)
+{
+}
+
+PunchCollider::~PunchCollider()
+{
+}
+
+void PunchCollider::Init()
+{
+}
+
+void PunchCollider::End()
+{
+}
+
+void PunchCollider::Update()
+{
+}
+
+void PunchCollider::Draw()
+{
+#ifdef _DEBUG
+	m_sphere.Draw();
+#endif
+}
+
+void PunchCollider::OnCollision(const GameObject& other)
+{
+}
---
+++ Project/source/GameObjects/PunchCollider.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "GameObject.h"
+
+class PunchCollider : public GameObject
+{
+public:
+	PunchCollider();
+	virtual ~PunchCollider() override;
+
+	void Init() override;
+	void End() override;
+	void Update() override;
+	void Draw() override;
+	
+	void OnCollision(const GameObject& other) override;
+
+private:
+
+};
+
