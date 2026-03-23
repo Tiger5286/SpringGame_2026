80b1aa30aff28925820f8c6ff804318ba00fa384
Tiger5286 <210645128+Tiger5286@users.noreply.github.com>
2026-03-23 23:25:25 +0900

    ChestManager実装

---
+++ Project/source/Managers/ChestManager.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <list>
+#include <memory>
+#include "../Utility/Vector3.h"
+
+class ModelManager;
+class CoinManager;
+class Chest;
+
+class ChestManager
+{
+public:
+	ChestManager(ModelManager& modelManager, CoinManager& coinManager);
+	virtual ~ChestManager();
+
+	void Init();
+	void End();
+	void Update();
+	void Draw();
+
+	void Spawn(const Vector3& playerPos);
+
+private:
+	ModelManager& m_modelManager;
+	CoinManager& m_coinManager;
+
+	std::list<std::shared_ptr<Chest>> m_chests;
+};
+
