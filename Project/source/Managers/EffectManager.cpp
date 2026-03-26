6cfd47cd51fe08dd4a3c13c9ebf13915eae30a90
Tiger5286 <210645128+Tiger5286@users.noreply.github.com>
2026-03-26 23:11:49 +0900

    EffectManager実装中

--- Project/source/Managers/EffectManager.cpp
+++ Project/source/Managers/EffectManager.cpp
@@ -1,1 +1,38 @@
-#include "EffectManager.h"
+﻿#include "EffectManager.h"
+#include "EffekseerForDXLib.h"
+
+EffectManager::EffectManager()
+{
+}
+
+EffectManager::~EffectManager()
+{
+}
+
+void EffectManager::Init()
+{
+}
+
+void EffectManager::End()
+{
+}
+
+void EffectManager::Update()
+{
+}
+
+void EffectManager::Draw()
+{
+}
+
+void EffectManager::LoadEffect(const std::wstring& filePath, const std::wstring& name, float scale)
+{
+	if (m_effectResourceHandles.contains(name))
+	{
+		assert(false && "登録するエフェクトの名前が重複しています");
+		return;
+	}
+	auto handle = LoadEffekseerEffect(filePath.c_str(), scale);
+	assert(handle != -1 && "エフェクトが正しくロードされませんでした");
+	m_effectResourceHandles[name] = handle;
+}
--- Project/source/Managers/EffectManager.h
+++ Project/source/Managers/EffectManager.h
@@ -1,5 +1,28 @@
-#pragma once
+﻿#pragma once
+#include <string>
+#include <list>
+#include <map>
+
 class EffectManager
 {
-};
+public:
+	EffectManager();
+	virtual ~EffectManager();
 
+	void Init();
+	void End();
+	void Update();
+	void Draw();
+
+	/// <summary>
+	/// エフェクトをロードし登録する
+	/// </summary>
+	/// <param name="filePath">ロードするエフェクトのファイルパス</param>
+	/// <param name="name">登録する名前</param>
+	/// <param name="scale">エフェクトの拡大率</param>
+	void LoadEffect(const std::wstring& filePath, const std::wstring& name,float scale = 1.0f);
+
+private:
+	std::map<std::wstring, int> m_effectResourceHandles;
+	std::list<int> m_effectPlayingHandles;
+};
