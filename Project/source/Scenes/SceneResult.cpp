cdb1acf3d67e9a513959bb6c45ea14f20cd3efdc
Tiger5286 <210645128+Tiger5286@users.noreply.github.com>
2026-03-29 20:45:26 +0900

    SceneResultクラス作成

---
+++ Project/source/Scenes/SceneResult.cpp
@@ -0,0 +1,26 @@
+﻿#include "SceneResult.h"
+
+SceneResult::SceneResult(Input& input):
+	SceneBase(input)
+{
+}
+
+SceneResult::~SceneResult()
+{
+}
+
+void SceneResult::Init()
+{
+}
+
+void SceneResult::End()
+{
+}
+
+void SceneResult::Update()
+{
+}
+
+void SceneResult::Draw()
+{
+}
---
+++ Project/source/Scenes/SceneResult.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "SceneBase.h"
+class SceneResult :
+    public SceneBase
+{
+public:
+	SceneResult(Input& input);
+	virtual ~SceneResult() override;
+
+	virtual void Init() override;
+	virtual void End() override;
+	virtual void Update() override;
+	virtual void Draw() override;
+
+private:
+
+};
+
