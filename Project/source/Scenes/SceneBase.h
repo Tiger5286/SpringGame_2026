1ba03850f2185b5b4726818277cfe9a4637abb03
Tiger5286 <210645128+Tiger5286@users.noreply.github.com>
2026-03-29 19:42:14 +0900

    SceneBaseクラス作成

---
+++ Project/source/Scenes/SceneBase.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+class SceneBase
+{
+public:
+	SceneBase();
+	virtual ~SceneBase();
+
+	virtual void Init() abstract;
+	virtual void End() abstract;
+	virtual void Update() abstract;
+	virtual void Draw() abstract;
+
+private:
+
+};
+
